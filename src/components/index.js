cffa67d59b63802cd0b290967d60f4be0213a8f3
Roman <kseexy@mail.ru>
2021-09-30 17:18:12 +0300

    added exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Input } from "./FormControl/Input/Input";
-export { default as Searchbar } from "./Searchbar/Searchbar";
 export { default as Header } from "./Header/Header";
 export { default as CoinsList } from "./CoinsList/CoinsList";
 export { default as SearchParams } from "./SearchParams/SearchParams";
@@ -8,3 +7,4 @@ export { default as Label } from "./FormControl/Label/Label";
 export { default as Button } from "./Buttons/Button/Button";
 export { default as SearchButton } from "./Buttons/SearchButton/SearchButton";
 export { default as Logo } from "./Logo/Logo";
+export { default as Navigation } from "./Navigation/Navigation";
