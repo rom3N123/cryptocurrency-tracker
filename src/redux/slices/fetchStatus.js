1350fd9219e228fa5a2f6776357b5ab70c96d7a1
Roman <kseexy@mail.ru>
2021-09-30 14:11:52 +0300

    made fetch status sluce

--- src/redux/slices/fetchStatus.js
+++ src/redux/slices/fetchStatus.js
@@ -1,1 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+   coins: false,
+};
+
+const fetchStatusSlice = createSlice({
+   name: "FETCH_STATUS",
+   initialState,
+   reducers: {
+      CHANGE_FETCH_STATUS: (state, action) => ({
+         ...state,
+         [action.payload]: !state[action.payload],
+      }),
+   },
+});
+
+export const { CHANGE_FETCH_STATUS } = fetchStatusSlice.actions;
+
+export default fetchStatusSlice.reducer;
