51bb3cd169b2d26c071c9620025e8d3d401f8233
Roman <kseexy@mail.ru>
2021-09-30 15:15:36 +0300

    deleted pagination slice and hook

---
+++ src/redux/slices/searchParams.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+   order: "market_cap_desc",
+   paginationSize: 30,
+   vs_currency: "usd",
+   category: null,
+};
+
+const searchParamsSlice = createSlice({
+   name: "SEARCH_PARAMS",
+   initialState,
+   reducers: {
+      SET_CURRENCY: (state, action) => ({
+         ...state,
+         category: action.payload,
+      }),
+
+      SET_PAGINATION_SIZE: (state, action) => ({
+         ...state,
+         paginationSize: action.payload,
+      }),
+
+      SET_ORDER: (state, action) => ({
+         ...state,
+         order: action.payload,
+      }),
+
+      SET_CATEGORY: (state, action) => ({
+         ...state,
+         category: action.payload,
+      }),
+   },
+});
+
+export const { SET_CATEGORY, SET_CURRENCY, SET_ORDER, SET_PAGINATION_SIZE } =
+   searchParamsSlice.actions;
+
+export default searchParamsSlice.reducer;
