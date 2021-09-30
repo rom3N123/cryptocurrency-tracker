338185c4cd423859daf664f1b044140a2440be54
Roman <kseexy@mail.ru>
2021-09-30 11:38:31 +0300

    made exports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Route, Switch } from "react-router";
+
 function App() {
-   return <div></div>;
+   return (
+      <div>
+         <Switch>
+            <Route exact path="/" component="" />
+         </Switch>
+      </div>
+   );
 }
 
 export default App;
