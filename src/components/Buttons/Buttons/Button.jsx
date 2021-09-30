d9c2394ab17c80a517014bb6c39738befb05d39b
Roman <kseexy@mail.ru>
2021-09-30 16:19:55 +0300

    made button component

---
+++ src/components/Buttons/Buttons/Button.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from "classnames";
+
+function Button({ className, icon = null, label }) {
+   return (
+      <button
+         className={classNames(`button ${className}`, {
+            "with-content": icon,
+         })}
+      >
+         {icon ? (
+            <div className="button__content">
+               <span>{icon}</span>
+               <span>{label}</span>
+            </div>
+         ) : (
+            label
+         )}
+      </button>
+   );
+}
+
+export default Button;
