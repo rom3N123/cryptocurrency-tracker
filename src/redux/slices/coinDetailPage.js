345fcdc60e40854cd8d775a8d14163ad5a48037a
Roman <kseexy@mail.ru>
2021-10-01 15:46:21 +0300

    refactored coin detail page reducer

--- src/redux/slices/coinDetailPage.js
+++ src/redux/slices/coinDetailPage.js
@@ -9,18 +9,19 @@ const coinDetailPageSlice = createSlice({
    name: "COIN_DETAIL_PAGE",
    initialState,
    reducers: {
-      SET_INFO: (state, action) => ({
+      SET_COIN_INFO: (state, action) => ({
          ...state,
          info: action.payload,
       }),
 
-      SET_MARKET_DATA: (state, action) => ({
+      SET_COIN_MARKET_DATA: (state, action) => ({
          ...state,
          marketData: action.payload,
       }),
    },
 });
 
-export const { SET_INFO, SET_MARKET_DATA } = coinDetailPageSlice.actions;
+export const { SET_COIN_INFO, SET_COIN_MARKET_DATA } =
+   coinDetailPageSlice.actions;
 
 export default coinDetailPageSlice.reducer;
