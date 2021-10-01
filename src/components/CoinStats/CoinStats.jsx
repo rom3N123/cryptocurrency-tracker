274d2bc86ca92c79cafb933c0f8104c50a61ae14
Roman <kseexy@mail.ru>
2021-10-01 16:58:37 +0300

    made coin stats component

---
+++ src/components/CoinStats/CoinStats.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { InformationBox } from "components";
+import { useSelector } from "react-redux";
+
+import "./CoinStats.scss";
+
+function CoinStats() {
+   const coin = useSelector((state) => state.coinDetailPage.info);
+
+   return (
+      <div className="coin-stats">
+         {coin.market_cap_rank && (
+            <InformationBox text={`Rank #` + coin.market_cap_rank} />
+         )}
+
+         {coin.hashing_algorithm && (
+            <InformationBox text={coin.hashing_algorithm} />
+         )}
+
+         {coin.categories.length && (
+            <InformationBox text={coin.categories[0]} />
+         )}
+      </div>
+   );
+}
+
+export default CoinStats;
