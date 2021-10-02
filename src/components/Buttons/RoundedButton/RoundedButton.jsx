63616bebdf7fe7598c734db63dff3ccfbd252bf5
Roman <kseexy@mail.ru>
2021-10-02 11:02:54 +0300

    made rounded button componnet

---
+++ src/components/Buttons/RoundedButton/RoundedButton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "@material-ui/styled-engine";
+import { Button } from "@material-ui/core";
+
+const RoundedBtn = styled(Button)(({ theme }) => ({
+   borderRadius: "8px",
+   textTransform: "none",
+   backgroundColor: theme.palette.primary.light,
+   color: "#fff",
+   padding: "4px 8px",
+
+   ".MuiSvgIcon-root": {
+      fontSize: "16px",
+   },
+
+   "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+   },
+}));
+
+function RoundedButton({ ...props }) {
+   return <RoundedBtn {...props} />;
+}
+
+export default RoundedButton;
