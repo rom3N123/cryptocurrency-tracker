207210685a2e0c088509faf13c381e471f69b820
Roman <kseexy@mail.ru>
2021-10-02 13:11:05 +0300

    refactored get domain

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -1,5 +1,7 @@
 const getDomain = (url) => {
-   return url.match(/[a-zA-Z]+.(org|com|ru|net)/)[0];
+   return url.match(
+      /\b(?!(?:https?|ftps?|http?):\/\/)(?!www\.)(?:[0-9A-Za-z][0-9A-Za-z-]{0,62})(?:\.(?:[0-9A-Za-z][0-9A-Za-z-]{0,62}))*(?:\.?|\b)/
+   )[0];
 };
 
 export default getDomain;
