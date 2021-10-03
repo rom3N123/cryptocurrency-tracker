44c1d611bb7a24f6249608528d8c9a9027ac70f5
Roman <kseexy@mail.ru>
2021-10-03 10:52:32 +0300

    changed chart styles

--- src/charts/defaultDatasetsStyles.js
+++ src/charts/defaultDatasetsStyles.js
@@ -2,10 +2,10 @@ const defaultDatasetsStyles = {
    fill: true,
    backgroundColor: "rgba(66, 165, 245, .4)",
    borderColor: "#1976d2",
-   borderWidth: 2,
+   borderWidth: 1,
    pointBorderColor: "#fff",
    pointBackgroundColor: "#1976d2",
-   pointHoverRadius: 7,
+   pointHoverRadius: 5,
    pointRadius: 3,
 };
 
