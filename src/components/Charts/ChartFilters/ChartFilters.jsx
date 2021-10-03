e1ceabe2459e40bd6ba92c7f7b0298b0d12376e0
Roman <kseexy@mail.ru>
2021-10-03 10:35:18 +0300

    made chart filters view

--- src/components/Charts/ChartFilters/ChartFilters.jsx
+++ src/components/Charts/ChartFilters/ChartFilters.jsx
@@ -4,15 +4,61 @@ import { ToggleButton, ToggleButtonGroup } from "@material-ui/core";
 import "./ChartFilters.scss";
 
 function ChartFilters() {
-   const filters = [
+   const typeFilters = [
       {
          name: "Price",
+         value: "prices",
+      },
+      {
+         name: "Market cap",
+         value: "market_caps",
+      },
+      {
+         name: "Total volumes",
+         value: "total_volumes",
+      },
+   ];
+
+   const periodFilters = [
+      {
+         name: "1D",
+         value: "1",
+      },
+      {
+         name: "7D",
+         value: "7",
+      },
+      {
+         name: "1M",
+         value: "30",
+      },
+      {
+         name: "2M",
+         value: "60",
+      },
+      {
+         name: "3M",
+         value: "90",
       },
    ];
 
    return (
       <div className="chart-filters">
-         <ToggleButtonGroup></ToggleButtonGroup>
+         <ToggleButtonGroup>
+            {typeFilters.map((filter) => (
+               <ToggleButton value={filter.value} key={filter.name}>
+                  {filter.name}
+               </ToggleButton>
+            ))}
+         </ToggleButtonGroup>
+
+         <ToggleButtonGroup>
+            {periodFilters.map((filter) => (
+               <ToggleButton value={filter.value} key={filter.name}>
+                  {filter.name}
+               </ToggleButton>
+            ))}
+         </ToggleButtonGroup>
       </div>
    );
 }
