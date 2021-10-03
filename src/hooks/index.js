f52b9206c74d3d5e9324338383a0943a1015f02e
Roman <kseexy@mail.ru>
2021-10-03 10:06:22 +0300

    added exports

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useApi } from "./useApi";
 export { default as useAsync } from "./useAsync";
 export { default as useSearchParams } from "./useSearchParams";
 export { default as useQuery } from "./useQuery";
+export { default as useChart } from "./useChart";
--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -2,4 +2,3 @@ export { default as fetchStatus } from "./fetchStatus";
 export { default as coins } from "./coins";
 export { default as searchParams } from "./searchParams";
 export { default as coinDetailPage } from "./coinDetailPage";
-export { default as chart } from "./chart";
