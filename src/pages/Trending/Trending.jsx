63ccac8bbd3c5147902b40b20d31b39a316dbeea
Roman <kseexy@mail.ru>
2021-10-03 12:07:28 +0300

    made trending list

---
+++ src/pages/Trending/Trending.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+   List,
+   ListItem,
+   ListItemButton,
+   ListItemIcon,
+   ListItemText,
+} from "@material-ui/core";
+import { useApi } from "hooks";
+
+import "./Trending.scss";
+
+function Trending() {
+   const { fetchTrending } = useApi();
+
+   const [trending, setTrending] = React.useState(null);
+
+   console.log(trending);
+
+   React.useEffect(() => {
+      const getTrendingItems = async () => {
+         const data = await fetchTrending();
+
+         setTrending(data);
+      };
+
+      getTrendingItems();
+   }, []);
+
+   return (
+      <section className="trending">
+         <div className="container">
+            <h3 className="trending__title">Trending</h3>
+            {trending && (
+               <List sx={{ maxWidth: "50%", margin: "0 auto" }}>
+                  {trending.map((el) => (
+                     <ListItem disablePadding>
+                        <ListItemButton>
+                           <ListItemIcon>
+                              <img src={el.item.thumb} alt="Crypto logo" />
+                           </ListItemIcon>
+
+                           <ListItemText sx={{ fontSize: "18px" }}>
+                              {el.item.name} #{el.item.market_cap_rank}
+                           </ListItemText>
+                        </ListItemButton>
+                     </ListItem>
+                  ))}
+               </List>
+            )}
+         </div>
+      </section>
+   );
+}
+
+export default Trending;
