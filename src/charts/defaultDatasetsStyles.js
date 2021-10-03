61e25be98aca209493a74e0a0b792b1770291568
Roman <kseexy@mail.ru>
2021-10-03 10:23:34 +0300

    refactored chart config

---
+++ src/charts/defaultDatasetsStyles.js
@@ -0,0 +1,12 @@
+const defaultDatasetsStyles = {
+   fill: true,
+   backgroundColor: "rgba(66, 165, 245, .4)",
+   borderColor: "#1976d2",
+   borderWidth: 2,
+   pointBorderColor: "#fff",
+   pointBackgroundColor: "#1976d2",
+   pointHoverRadius: 7,
+   pointRadius: 3,
+};
+
+export default defaultDatasetsStyles;
