d455be36d2147875c3a9aa248e007fa170b02175
Roman <kseexy@mail.ru>
2021-10-03 09:54:35 +0300

    added chart controls for search params hook

--- src/hooks/useSearchParams.js
+++ src/hooks/useSearchParams.js
@@ -1,6 +1,8 @@
 import { useDispatch } from "react-redux";
 import {
    SET_CATEGORY,
+   SET_CHART_PERIOD,
+   SET_CHART_TYPE,
    SET_CURRENCY,
    SET_ORDER,
    SET_PAGINATION_SIZE,
@@ -17,11 +19,17 @@ const useSearchParams = () => {
 
    const setCategory = (category) => dispatch(SET_CATEGORY(category));
 
+   const setChartPeriod = (period) => dispatch(SET_CHART_PERIOD(period));
+
+   const setChartType = (type) => dispatch(SET_CHART_TYPE(type));
+
    return {
       setCurrency,
       setPaginationSize,
       setOrder,
       setCategory,
+      setChartPeriod,
+      setChartType,
    };
 };
 
